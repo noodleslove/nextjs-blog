3a179e85a1605b225a7fd3ceaa47f7bdabee42a4
Eddie Ho <noodleslove@icloud.com>
2022-08-30 15:43:22 -0700

    Add outputStandalone for docker

--- next.config.js
+++ next.config.js
@@ -54,6 +54,9 @@ const securityHeaders = [
 
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
+  experimental: {
+    outputStandalone: true,
+  },
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
   eslint: {
     dirs: ['pages', 'components', 'lib', 'layouts', 'scripts'],
