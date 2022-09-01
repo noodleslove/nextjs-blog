0f07f08a20dbf204a5defa316130ff2e763c58c7
Eddie Ho <noodleslove@icloud.com>
2022-09-01 13:16:39 -0700

    Add blockchain-in-go

--- data/projectsData.js
+++ data/projectsData.js
@@ -8,12 +8,10 @@ const projectsData = [
     href: 'https://www.google.com',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Blockchain in Go',
+    description: `A simplified blockchain implementation in Golang`,
+    imgSrc: '/static/images/blockchain.jpg',
+    href: 'https://github.com/noodleslove/blockchain-go',
   },
 ]
 
