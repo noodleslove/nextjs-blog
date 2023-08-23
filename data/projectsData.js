d8d3ff258a5551423545f207ddb395275f9dab58
Eddie Ho <noodleslove@icloud.com>
2023-08-23 12:51:47 +0800

    Organized personal projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,5 +1,11 @@
 const projectsData = [
   {
+    title: 'UCSD CSES Website',
+    description: `A website for UCSD CSES, a student organization at UC San Diego.`,
+    imgSrc: '/static/images/ucsd-cses.jpg',
+    href: 'https://github.com/Will-Hsu/cses_webdev',
+  },
+  {
     title: 'NutriPal',
     description:
       'NutriPal is a user-friendly diet information website that aims to simplify the daily diet logging process.',
@@ -7,10 +13,25 @@ const projectsData = [
     href: 'https://github.com/noodleslove/nutriPal',
   },
   {
-    title: 'House of Representatives Analysis',
-    description: `This project uses public data about the stock trades made by members of the US House of Representatives.`,
+    title: 'LFM Recommender System',
+    description:
+      'A Yelp Recommender System using Latent Factor Collaborative Filtering to provide personalized business recommendations through a web application, enhancing user experience and engagement.',
+    imgSrc: '/static/images/lfm.jpg',
+    href: 'https://drive.google.com/file/d/1cbem5KYzXJhihUirzvo_5JAMdW6Dw8LF/view?usp=sharing',
+  },
+  {
+    title: 'House of Representatives Analysis II',
+    description:
+      'This project analyzes stock trades made by members of the 116th U.S. House of Representatives, combining it with information on their political affiliation to evaluate missingness associations in the dataset.',
+    imgSrc: '/static/images/stock-2.jpg',
+    href: 'https://drive.google.com/file/d/10AgiJjXxIM7ZC-H-ScfD9iZHK3lsnAdt/view?usp=sharing',
+  },
+  {
+    title: 'House of Representatives Analysis I',
+    description:
+      'This project analyzes stock trades made by members of the 116th U.S. House of Representatives, combining it with information on their political affiliation to evaluate missingness associations in the dataset.',
     imgSrc: '/static/images/stock.png',
-    href: 'https://github.com/noodleslove/House-of-Representative-Analysis-I',
+    href: 'https://drive.google.com/file/d/1M3fHZAFZXAAp5A9QwROB91KcIcK2SG03/view?usp=sharing',
   },
   {
     title: 'Blockchain in Go',
